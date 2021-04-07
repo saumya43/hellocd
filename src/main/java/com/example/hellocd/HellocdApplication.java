bc516700e1102eeeabb0f6d883008619de9dfc79
Saumya <Saumya043@gmail.com>
2021-04-07 16:25:19 -0700

    added for continuous delivery

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -14,7 +14,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "we are working on continuous delivery!";
 	}
 
 	public static void main(String[] args) {
